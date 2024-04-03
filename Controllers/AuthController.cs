7f852953125d3e61f6000ed2c0ab435b6b8b43b1
Alon Kolyakov <alon.kolyakov@abra.co.il>
2024-04-03 15:46:18 +0300

    [Test]

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -2,27 +2,26 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 
-namespace DevaloreAssignment.Controllers
+namespace DevaloreAssignment.Controllers;
+
+public class AuthController : Controller
 {
-    public class AuthController : Controller
+    [HttpPost("/mvc/login")]
+    public async Task<IActionResult> Login()
     {
-        [HttpPost("/mvc/login")]
-        public async Task<IActionResult> Login()
-        {
-            await HttpContext.SignInAsync("default", new ClaimsPrincipal(
-                new ClaimsIdentity(
-                    new Claim[]
-                    {
-                        new Claim(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString()),
-                    },
-                    "default"
-                    )
-                ),
-                new AuthenticationProperties
+        await HttpContext.SignInAsync("default", new ClaimsPrincipal(
+            new ClaimsIdentity(
+                new Claim[]
                 {
-                    IsPersistent = true,
-                });
-            return Ok();
-        }
+                    new Claim(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString()),
+                },
+                "default"
+                )
+            ),
+            new AuthenticationProperties
+            {
+                IsPersistent = true,
+            });
+        return Ok();
     }
 }
